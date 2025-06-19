620ab3eb732c47558c62acd1d349042daff19c58
Anastasia Florentis <anastasia.florentis@Mac.lan>
2025-06-19 17:18:02 +0100

    work on mvp

---
+++ src/test/scala/stepdefs/FormSubmissionSteps.scala
@@ -0,0 +1,72 @@
+package stepdefs
+
+import io.cucumber.scala.{EN, ScalaDsl}
+import pages.PracticeFormPage
+import pages.PracticeFormPage.{acceptCookies, clickSubmit, enterDate, inputFirstName, inputLastName, launchBrowser, selectAutomationTools, selectContinent, selectExperience, selectGender, selectProfessions, uploadImage}
+
+class FormSubmissionSteps extends ScalaDsl with EN {
+
+  Given("""the user is on the practice form page""") { () =>
+    launchBrowser()
+    println("Browser launched")
+  }
+
+  And("""accepts the webpage cookies""") { () =>
+    acceptCookies()
+    println("accepted cookies")
+  }
+
+  And("""enters a first name""") { () =>
+    inputFirstName("Jane")
+    println("Entered first name")
+  }
+
+  And("""enters a last name""") { () =>
+    inputLastName("Doe")
+    println("Entered last name")
+  }
+
+  And("""selects a gender radio button""") { () =>
+    selectGender("Female")
+    println("Gender selected")
+  }
+
+  And("""selects an experience radio button""") { () =>
+    selectExperience(4)
+    println("Selected experience")
+  }
+
+  And("""enters a date""") { () =>
+    enterDate("19/06/2025")
+    println("Entered date")
+  }
+
+  And("""checks a profession""") { () =>
+    selectProfessions(List("automation tester"))
+    println("Chose profession")
+  }
+
+  And("""chooses automation tools""") { () =>
+    selectAutomationTools(List("qtp", "selenium webdriver"))
+    println("chose automation tools ")
+  }
+
+  And("""chooses a continent""") { () =>
+    selectContinent("Europe")
+    println("chose continent ")
+  }
+
+  And("""uploads a file""") { () =>
+    uploadImage("/Users/anastasia.florentis/Desktop/testImage.jpg")
+    println("uploaded image")
+  }
+
+  When("""the user clicks on the submit button""") { () =>
+    clickSubmit()
+  }
+
+  Then("""the a line should be printed saying "button found"""") { () =>
+    println("Submit button found and 'clicked'")
+  }
+
+}
--- src/test/scala/support/Hooks.scala
+++ src/test/scala/support/Hooks.scala
@@ -1,6 +1,7 @@
 package support
 
 import io.cucumber.scala.{EN, ScalaDsl}
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 
 
@@ -9,14 +10,29 @@ class Hooks extends ScalaDsl with EN {
   val options = new ChromeOptions()
   options.addArguments("--headless=new")
 
-  Before {
+  Before("@login") {
     println("Launching browser before scenario...")
     DriverManager.driver = new ChromeDriver(options)
     DriverManager.driver.manage().window().maximize()
   }
 
-  After {
+  After("@login") {
     println("Closing browser after scenario...")
     DriverManager.driver.quit()
   }
+
+  Before("@form") {
+    println("Launching *form* browser before scenario...")
+//    // initialize driver
+//    val driver: WebDriver = new ChromeDriver()
+//    driver.manage().window().maximize()
+//    driver.get("https://www.techlistic.com/p/selenium-practice-form.html")
+  }
+
+  After("@form") {
+    println("Closing *form* browser after scenario...")
+//    DriverManager.driver.quit()
+  }
+
+
 }
